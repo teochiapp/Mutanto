a07a2418013b61cf06dbcc2378635d62be2fbef4
Teo <teochiapps@gmail.com>
2026-09-08 14:51:25 -0300

    asd

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
      //COMPAY NAME
      define("COMPANY_NAME","Mutanto");
      //SITE URL
-     define("URL_SITE","http://localhost:8000/");
+     define("URL_SITE","https://mutanto.com.ar/");
      //Dependences
      require_once(realpath($_SERVER['DOCUMENT_ROOT']."/dependencys/dependencys.php"));
      require_once(realpath($_SERVER['DOCUMENT_ROOT']  . "/language/select.php"));
